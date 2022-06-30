aa00a7f2500ff97082ce90e48dcd66d42208208b
missrr87 <misrades@gmail.com>
2022-06-30 15:02:43 -0700

    print a message using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+#include <stdio.h>
+
+/* 
+ * my first 
+ *
+ * c
+ * programmminf 
+ */
+
+init main ()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+        return 0;
+  
+}	       
