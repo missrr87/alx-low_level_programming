bd1a505973faddca035c618ac806b8880f82aa5b
missrr87 <misrades@gmail.com>
2022-07-01 14:52:09 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - 0 to 10
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int ch;
+for (ch = 47; ch <= 57; ch++)
+{
+putchar(ch);
+}
+putchar("\n");
+return (0);
+}
