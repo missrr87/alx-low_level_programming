d51c09954fa04125e8797c53df023e9f639d1746
missrr87 <misrades@gmail.com>
2022-07-01 14:03:27 -0700

    except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,20 +2,19 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - print all small + big letters
+ * main - print all small except q and e
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
 int ch;
 for (ch = 97; ch <= 122; ch++)
 {
-putchar(ch);
-}
-for (ch = 65; ch <= 90; ch++)
+if (ch != 101 && ch != 113)
 {
 putchar(ch);
+}
 }
 putchar(10);
 return (0);
