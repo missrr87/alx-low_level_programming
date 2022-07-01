6490c2e6cf85fe31cbb14fb406aeead3b3cd18a0
missrr87 <misrades@gmail.com>
2022-07-01 10:30:19 -0700

    hot fix

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,13 +13,13 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%n is positive", n);
+printf("%d is positive", n);
 }
 if (n == 0)
 {
-printf("%n is zero\n", n);
+printf("%d is zero\n", n);
 if (n < 0)
 {
-printf("%n is negative\n", n);
+printf("%d is negative\n", n);
 return (0);
 }
