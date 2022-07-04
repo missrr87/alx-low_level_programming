84d60f725ae25cbae643fb8fab7aa7c9a9a48489
missrr87 <misrades@gmail.com>
2022-07-04 01:31:33 -0700

    hot fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 int num;
-for (num = 47; num <= 57; num++)
+for (num = 48; num <= 57; num++)
 {
 putchar(num);
 if (num != 57)
@@ -17,6 +17,6 @@ putchar(44);
 putchar(32);
 }
 }
-putchar (10);
+putchar(10);
 return (0);
 }
