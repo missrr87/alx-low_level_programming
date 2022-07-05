148c60bfa46e306e5ac000f861498db6b7381682
missrr87 <misrades@gmail.com>
2022-07-05 06:54:58 -0700

    check wheather alphabet or not

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - checks if c is alphabet or not
+ * @c : a character
+ *
+ * Return: 1 (success); 0 (not success)
+ */
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z')
+||
+(c >= 'A' && c <= 'Z')
+return (1);
+}
