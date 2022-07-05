ea4e989bc584b0e980a632410c2bf77f095a06dc
missrr87 <misrades@gmail.com>
2022-07-05 07:16:10 -0700

    positive, zero and negative

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - checks if a number is
+ * negative, positive or zero
+ *@n : an intiger
+ *
+ *Return: 1 (for positive) 0 (for zero)
+ *-1 (for negative)
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+}
+if (n == 0)
+{
+return (0);
+}
+else
+{
+return (-1);
+}
+}
