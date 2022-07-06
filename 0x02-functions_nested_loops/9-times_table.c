5b7b547c8a44086c3d07bce4f37af486a3699a5b
missrr87 <misrades@gmail.com>
2022-07-06 03:07:13 -0700

    9 by 9 multiplication

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * times_table - multiplication table 9 by 9
+ */
+void times_table(void)
+{
+int n;
+int m;
+int p;
+for (n = 0; n < 10; n++)
+{
+for (m = 0; m < 10; m++)
+{
+_putchar('0');
+_putchar(',');
+_putchar(' ');
+p = n * m;
+if (p < 10)
+{
+_putchar(' ');
+_putchar(p + '0');
+}
+else
+{
+_putchar((p / 10) + '0');
+_putchar((p % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
