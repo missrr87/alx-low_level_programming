cc1eb78fa2ed5a209ca5f741fe976f2934656c4a
missrr87 <misrades@gmail.com>
2022-07-06 03:40:56 -0700

    fix fixy

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,18 +12,23 @@ for (n = 0; n < 10; n++)
 for (m = 0; m < 10; m++)
 {
 p = n * m;
+if (p == 0)
+{
+_putchar(P + '0');
+}
 if (p < 10)
 {
-_putchar(p + '0');
 _putchar(',');
 _putchar(' ');
+_putchar(' ');
+_putchar(p + '0');
 }
 else
 {
-_putchar((p / 10) + '0');
-_putchar((p % 10) + '0');
 _putchar(',');
 _putchar(' ');
+_putchar((p / 10) + '0');
+_putchar((p % 10) + '0');
 }
 }
 _putchar('\n');
